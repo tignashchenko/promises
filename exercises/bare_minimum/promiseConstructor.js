1283e44f362f9f5ba77e8898cac6d678528d021f
Taras Ignashchenko <tignashchenko@gmail.com>
2017-06-10 22:31:40 -0700

    Implement getStatusCodeAsync promiseConstructor function and pass its tests

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -23,7 +23,15 @@ var pluckFirstLineFromFileAsync = function(filePath) {
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise(function(resolve, reject) {
+    request(url, function(err, res, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res.statusCode);
+      }
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
