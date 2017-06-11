f2c70867e3031926e8bb6fba53741c17c449b78c
Taras Ignashchenko <tignashchenko@gmail.com>
2017-06-10 21:31:29 -0700

    Implement getStatusCode function and pass all its tests

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -17,8 +17,14 @@ var pluckFirstLineFromFile = function (filePath, callback) {
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request(url, function(err, res, body) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, res.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
